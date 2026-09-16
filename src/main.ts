e547c9e933e15728e9e99310879650377049f952
sirfrajendra <sirfrajendra@gmail.com>
2026-09-16 12:34:52 +0545

    cors added

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  app.enableCors({
+    origin: 'http://localhost:3001',
+  });
+
   app.useGlobalPipes(new ValidationPipe({
     transform: true,
   }));
